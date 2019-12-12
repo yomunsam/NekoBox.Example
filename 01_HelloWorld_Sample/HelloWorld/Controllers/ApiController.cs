ec48403625875306da7ca1f273b593fc072bce57
yomunsam <yomunsam@outlook.com>
2019-12-12 21:01:13 +0800

    喵呜

---
+++ 01_HelloWorld_Sample/HelloWorld/Controllers/ApiController.cs
@@ -0,0 +1,25 @@
+﻿using NekoBox.Client;
+using NekoBox.Client.Message;
+using System.Threading.Tasks;
+
+namespace Nekonya.Example
+{
+    public class ApiController : MessageController
+    {
+        public ApiController(IGatewayBox gateway) : base(gateway) { }
+        
+
+        [GET("hello")]
+        public Task SayHello(IMessageRequest req, IMessageResponse resp)
+        {
+            return resp.SendAsync(new { msg = "hello , meow ~~!" });
+        }
+
+        [POST("meow")] //按照约定，GET和POST的一大区别是：使用POST方法时，客户端会在消息中携带参数。
+        public Task SayMeow(IMessageRequest req, IMessageResponse resp)
+        {
+            return resp.SendTextAsync(" meow~! meow~! " + req.GetTextMessage());
+        }
+
+    }
+}
---
+++ 01_HelloWorld_Sample/HelloWorld/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using NekoBox;
+using NekoBox.Client;
+using NekoBox.Http.Provider;
+using NekoBox.WebSocket.Provider;
+using NekoBox.Client.Message;
+
+namespace Nekonya.Example
+{
+    class Program
+    {
+        static int Main()
+        {
+            Console.WriteLine("Hello World!");
+            return DoMainAsync().Result;
+                
+        }
+
+        static async Task<int> DoMainAsync()
+        {
+            var builder = new GatewayBuilder()
+                .SetGatewayName("demo_gateway")
+                .ConfigureCluster(options =>
+                {
+                    options.DisableClustering();    //不使用集群模式
+                })
+                .SetHttpProvider(provider =>
+                {
+                    provider.EnableSSL = false;
+                    provider.GatewayUrl = "meow";
+                    provider.ListenIP = System.Net.IPAddress.Any;
+                    provider.Port = 80;
+                    provider.UseJson(); //使用JSON格式解析、编码消息
+                })
+                .SetWebSocketProvider(provider =>
+                {
+                    provider.EnableSSL = false;
+                    provider.HandleUrl = "meow";
+                    provider.HostAddress = System.Net.IPAddress.Any;
+                    provider.Port = 9002;
+                    provider.UseLibuv = true;
+                    provider.UseJson(); //使用JSON格式解析、编码消息
+                })
+                .AddMessageHandler(new MessageHandler("hello", MessageMethod.GET, (req, resp) =>
+                {
+                    return resp.SendAsync(new { msg = "hello" });
+                }))
+                .AddMessageHandler(new MessageHandler()
+                {
+                    ProtocolName = "player:*",
+                    Method = MessageMethod.POST,
+                    Handler = (req, resp) =>
+                    {
+                        return resp.SendAsync(new { msg = "hello", proto_name = req.ProtocolName });
+                    }
+                })
+                .AddMessageController<ApiController>("api:");
+
+
+
+            IGatewayBox gateway = await builder.RunAsync();
+
+            ReadInput();
+            return 0;
+        }
+
+
+        static void ReadInput()
+        {
+            Console.Write("Gateway>");
+            var str = Console.ReadLine();
+
+            //Do something
+
+            ReadInput();
+        }
+
+    }
+}
